6d1dfe7c838ead7af7131602b46862577dbb2d02
wangd1230 <790894413@qq.com>
2016-11-24 00:19:44 +0800

    first

---
+++ initLeanCloud.js
@@ -0,0 +1,8 @@
+// LeanCloud - 初始化 - 将这里的 APP_ID 和 APP_KEY 替换成自己的应用数据
+// https://leancloud.cn/docs/sdk_setup-js.html#初始化
+var APP_ID = 'C1ohpshVWpyJwdkbw52yqUdT-gzGzoHsz';
+var APP_KEY = '9p9ATwLmBmRAt2Pe0F1HTILt';
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
